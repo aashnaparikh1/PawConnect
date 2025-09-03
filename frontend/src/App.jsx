55b08f8300ec1e3b4e238d17ade1868c38430ab1
Ashutosh Rakesh Dosi <ashutoshdosi@gmail.com>
2025-09-03 15:39:43 +0530

    Tp update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,8 @@
-import NavBar from "./components/NavBar";
-import DemoAPI from "./DemoAPI";
 import DemoAPI2 from "./DemoAPI2";
 
 export default function App() {
   return (
     <>
-    <NavBar/>
-    
-  
       <DemoAPI2 />
     </>
   );
